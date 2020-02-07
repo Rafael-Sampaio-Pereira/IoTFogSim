b9b885a076cc02c1d465d93711b1114a9d6ffbd5
Rafael_sampaio <rafaelsampaiop@hotmail.com>
2020-02-07 16:01:19 -0300

    [cod] Salvandoultimas mudanças para começar a trabalhar na maquina do lactec

--- projects/example/connections.js
+++ projects/example/connections.js
@@ -2,7 +2,15 @@
 	"connections": [
 		{
 			"id_device_1": 1,
-			"id_device_2": 3
+			"id_device_2": 8
+		},
+		{
+			"id_device_1": 2,
+			"id_device_2": 8
+		},
+		{
+			"id_device_1": 3,
+			"id_device_2": 8
 		}
 	]
 }
