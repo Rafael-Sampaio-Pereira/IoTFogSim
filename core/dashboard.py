54bacef897ceec1636d6f65bcd0052fb71f99fcb
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-10-07 00:51:06 -0300

    [cod] Improving dashboard

--- core/dashboard.py
+++ core/dashboard.py
@@ -10,27 +10,28 @@ from PIL import ImageTk
 
 class DashboardScreen(tkinter.Frame):
     def __init__(self, project_name, simulation_core):
-        root = tkinter.Toplevel()
-        tksupport.install(root)
+        self.root = tkinter.Toplevel()
+        tksupport.install(self.root)
         w_heigth = 300
         w_width = 700
         w_top_padding = 900
         w_letf_padding = 1000
         self.scrollable_height = w_heigth + 200
-        root.geometry(str(w_width)+"x"+str(w_heigth)+"+" +
+        self.scrollable_width = w_width
+        self.root.geometry(str(w_width)+"x"+str(w_heigth)+"+" +
                         str(w_letf_padding)+"+"+str(w_top_padding))
-        root.resizable(False, True)
-        root.iconify()
-        tkinter.Frame.__init__(self, root)
+        self.root.resizable(False, True)
+        self.root.iconify()
+        tkinter.Frame.__init__(self, self.root)
         
-        root.bind("<F11>", lambda event: root.attributes("-fullscreen",
-                                                            not root.attributes("-fullscreen")))
-        root.iconphoto(True, PhotoImage(
+        self.root.bind("<F11>", lambda event: self.root.attributes("-fullscreen",
+                                                            not self.root.attributes("-fullscreen")))
+        self.root.iconphoto(True, PhotoImage(
             file='graphics/icons/iotfogsim_icon.png'))
-        root.title(
+        self.root.title(
             "IoTFogSim %s - An Event-Driven Network Simulator - Dashboard" % (version))
         
-        root.update()
+        self.root.update()
         self.canvas = tkinter.Canvas(
             self, width=500, height=400, bg='#263238', highlightthickness=0)
         self.canvas.simulation_core = simulation_core
@@ -43,7 +44,7 @@ class DashboardScreen(tkinter.Frame):
         self.canvas.configure(yscrollcommand=self.ysb.set,
                               xscrollcommand=None)
         self.canvas.configure(scrollregion=(
-            0, 0,  w_width,  self.scrollable_height))
+            0, 0,  self.scrollable_width,  self.scrollable_height))
 
         # self.xsb.grid(row=1, column=0, sticky="ew")
         self.ysb.grid(row=0, column=1, sticky="ns")
@@ -56,7 +57,6 @@ class DashboardScreen(tkinter.Frame):
         self.canvas.bind("<B2-Motion>", self.scroll_move)
         
         self.all_icons = []
-        self.default_icon = None
         
         update_interval = 1
         LoopingCall(self.update_dashboard).start(update_interval, now=True)
@@ -77,11 +77,25 @@ class DashboardScreen(tkinter.Frame):
     def update_dashboard(self):
         before_padding = 10
         last_height = 60
-        for machine in self.simulation_core.all_machines:
+
+        for index, machine in enumerate(self.simulation_core.all_machines):
             image_file = ImageTk.PhotoImage(file=machine.icon)
             temp_height = image_file.height()+before_padding
             self.scrollable_height += temp_height
-            self.default_icon  = self.canvas.create_image(
+            self.canvas.create_image(
                 50, last_height, image=image_file, tag="icon")
             self.all_icons.append(image_file)
             last_height += 60
+        
+        self.canvas.configure(scrollregion=(
+            0, 0,  self.scrollable_width,  self.scrollable_height))
+        
+
+            
+            # self.canvas.columnconfigure(0) # making the columns responsive
+            # self.canvas.rowconfigure(index) # making the rows responsive
+            # # b = tkinter.Entry(self.root,label=image_file)
+            # label = tkinter.Label(self.canvas, image = image_file)
+            # label.image = image_file
+            # label.grid(row=index,column=0,sticky=tkinter.NSEW)
+
