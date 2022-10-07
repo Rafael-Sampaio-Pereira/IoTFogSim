b6370db8222dc66a98b2a86b972693b79798ea89
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-10-06 23:08:24 -0300

    [cod] Improving dashboard base by adding dashboard scroll y

--- core/dashboard.py
+++ core/dashboard.py
@@ -14,6 +14,7 @@ class DashboardScreen(tkinter.Frame):
         w_width = 700
         w_top_padding = 900
         w_letf_padding = 1000
+        self.scrollable_height = w_heigth + 200
         root.geometry(str(w_width)+"x"+str(w_heigth)+"+" +
                         str(w_letf_padding)+"+"+str(w_top_padding))
         root.resizable(False, True)
@@ -30,10 +31,43 @@ class DashboardScreen(tkinter.Frame):
         root.update()
         self.canvas = tkinter.Canvas(
             self, width=500, height=400, bg='#263238', highlightthickness=0)
-        self.canvas.pack(side="top", fill="both", expand=True)
         self.canvas.simulation_core = simulation_core
+        self.simulation_core = simulation_core
         
-        self.ball = self.canvas.create_oval(10, 10, 40, 40, fill="black")
+        # self.xsb = tkinter.Scrollbar(
+        #     self, orient="horizontal", command=self.canvas.xview)
+        self.ysb = tkinter.Scrollbar(
+            self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.ysb.set,
+                              xscrollcommand=None)
+        self.canvas.configure(scrollregion=(
+            0, 0,  w_width,  self.scrollable_height))
+
+        # self.xsb.grid(row=1, column=0, sticky="ew")
+        self.ysb.grid(row=0, column=1, sticky="ns")
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        # self.canvas.create_text(50,10, anchor="nw", text="Events: ")
+        # self.events_counter_label = self.canvas.create_text(102,10, anchor="nw", text="0", tags=("events_counter_label",))
+
+        # # Label to show position on screen - Rafael Sampaio
+        # self.position_label = self.canvas.create_text(300,10, anchor="nw", text="0", tags=("position_label",))
+
+        # This is what enables scrolling with the mouse:
+        self.canvas.bind("<ButtonPress-2>", self.scroll_start)
+        self.canvas.bind("<B2-Motion>", self.scroll_move)
+        
+        self.ball = self.canvas.create_oval(100, 400, 150, 450, fill="red")
+        
+    def scroll_start(self, event):
+        self.canvas.config(cursor='fleur')
+        self.canvas.scan_mark(event.x, event.y)
+
+    def scroll_move(self, event):
+        self.canvas.config(cursor='fleur')
+        self.canvas.scan_dragto(event.x, event.y, gain=1)
         
     
     def getCanvas(self):
--- r.py
+++ r.py
@@ -13,75 +13,54 @@
 #     yield sleep(0.5)
 #     print('now i am back ...')
 
-import scipy.stats
-import random
-
-def simulate_network_delay(upper_bound, lower_bound, mean, standard_deviation, global_seed=None):
-    """
-    this function was based on the below stackoverflow answer, consider that 
-    omnet++ inet framework has proved that truncnorm distribution can be used 
-    to simulate network delay.
-    
-    INET documentation example:
-    https://github.com/inet-framework/inet/blob/master/examples/internetcloud/cloudandrouters/internetCloud.xml
-    
-    <internetCloud symmetric="true">
-        <parameters name="good">
-            <traffic src="srouter[0]" dest="rrouter" delay="20ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.01" />
-            <traffic src="srouter[1]" dest="rrouter" delay="30ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.02" />
-            <traffic src="srouter[2]" dest="rrouter" delay="40ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.03" />
-            <traffic src="srouter[3]" dest="rrouter" delay="50ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.04" />
-            <traffic src="srouter[4]" dest="rrouter" delay="60ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.05" />
-            <traffic src="srouter[5]" dest="rrouter" delay="70ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.06" />
-            <traffic src="srouter[6]" dest="rrouter" delay="80ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.07" />
-            <traffic src="srouter[7]" dest="rrouter" delay="90ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.08" />
-            <traffic src="srouter[8]" dest="rrouter" delay="100ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.09" />
-            <traffic src="srouter[9]" dest="rrouter" delay="110ms+truncnormal(200ms,60ms)" datarate="uniform(100kbps,1Mbps)" drop="uniform(0,1) &lt; 0.10" />
-            <!--
-                <traffic src="**" dest="**" delay="10ms+truncnormal(100ms,20ms)" datarate="uniform(100kbps,500kbps)" drop="uniform(0,1) &lt; uniform(0.01, 0.05)" />
-            -->
-        </parameters>
-    </internetCloud>
-    
-    https://stackoverflow.com/a/28013759
-    I came across this post while searching for a way to return a series of 
-    values sampled from a normal distribution truncated between zero and 1 
-    (i.e. probabilities). To help anyone else who has the same problem, I just
-    wanted to note that scipy.stats.truncnorm has the built-in capability ".rvs".
-    So, if you wanted 100,000 samples with a mean of 0.5 and standard deviation of 0.1:
-    
-    import scipy.stats
-    lower = 0
-    upper = 1
-    mu = 0.5
-    sigma = 0.1
-    N = 100000
-
-    samples = scipy.stats.truncnorm.rvs(
-            (lower-mu)/sigma,(upper-mu)/sigma,loc=mu,scale=sigma,size=N)
-    
-    This gives a behavior very similar to numpy.random.normal, but within the
-    bounds desired. Using the built-in will be substantially faster than
-    looping to gather samples, especially for large values of N.
-    
-    usage example:
-        delay = simulate_network_delay(200, 60, 70, 10)
-    
-    """
-    if global_seed:
-        random.seed(global_seed)
-    
-    mu = mean
-    sigma = standard_deviation
-    N = 100000 # number of samples
-    samples = scipy.stats.truncnorm.rvs(
-            (lower_bound-mu)/sigma,(upper_bound-mu)/sigma,loc=mu,scale=sigma,size=N)
-
-    sample = random.choice(samples)
-    delay = round((sample/1000),3)
-    return delay
-        
-        
-print(simulate_network_delay(200, 60, 70, 10))
+import tkinter
+import os
 
 
+class ClusterDialog(tkinter.Toplevel):
+    def __init__(self, parent, displayClass, clusterInfo, title = None):        
+        tkinter.Toplevel.__init__(self, parent)
+        self.transient(parent)
+        #top = self.top = Toplevel(parent)
+        if title:
+            self.title(title)
+        #set parent
+        self.parent = parent
+        #set class
+        self.dClass = displayClass
+        #dictionary to store the header data in 
+        self.clusterInfo    = clusterInfo        
+        self.geometry("300x300+10+10")
+
+        #stores checkbox variables
+        self.varList = None
+        self.boxList = None
+        self.name = None
+
+        self.frameTopLevel  = tkinter.Frame(self,bd=2, width = 200,height=300)
+        self.frameTopLevel.pack()
+
+        self.buttonbox(self.frameTopLevel)
+        self.frame = tkinter.Frame(self.frameTopLevel, width = 200,height=300)
+
+        #frame=Frame(root,width=300,height=300)
+        self.frame.grid(row=0,column=0)
+        self.frame.pack()
+
+        self.canvas=tkinter.Canvas(self.frame,bg='#FFFFFF',width=300,height=300,scrollregion=(0,0,500,1000))
+
+        hbar = tkinter.Scrollbar(self.frame,orient=tkinter.HORIZONTAL)
+        hbar.pack(side=tkinter.BOTTOM,fill=tkinter.X)
+        hbar.config(command=self.canvas.xview)
+        vbar=tkinter.Scrollbar(self.frame,orient=tkinter.VERTICAL)
+        vbar.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+        vbar.config(command=self.canvas.yview)
+        self.canvas.config(width=300,height=300)
+        self.canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        self.canvas.pack(side=tkinter.LEFT,expand=True,fill=tkinter.BOTH)
+        self.frame.config(height = 100)
+        self.body(self.canvas)
+        self.canvas.config(width=300,height=300)
+        self.grab_set()
+
+ClusterDialog()
